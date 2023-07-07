d22eb1632b3dc7e92878514fe9be68273fc5cbbd
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-07-07 11:20:01 +0200

    Show OSM background

--- src/index.js
+++ src/index.js
@@ -64,6 +64,13 @@ function init (err) {
 
   map = L.map('map', { maxZoom: 22 })
 
+  // Show OSM map background
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxNativeZoom: 19,
+      maxZoom: 22,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
   map.attributionControl.setPrefix('<a target="_blank" href="https://github.com/geowiki-net/geowiki-viewer/">geowiki-viewer</a>')
 
   if (window.location.search) {
