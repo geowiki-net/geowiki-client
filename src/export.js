5d475c7954a2068657ce4246fb245ae9c0a0bd5a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-01-22 22:05:40 +0100

    rewrite export.js

--- src/export.js
+++ src/export.js
@@ -1,10 +1,14 @@
 import App from './App'
-import lang from './lang'
-import map from './map'
-import data from './data'
-import layer from './layer'
-import config from './config'
 
-module.exports = {
-  App, lang, map, data, layer, config
-}
+const baseModules = [
+  require('./lang'),
+  require('./map'),
+  require('./data'),
+  require('./layer'),
+  require('./config'),
+  require('./styleLoader')
+]
+
+App.modules = [...baseModules, ...App.modules]
+
+module.exports = App
