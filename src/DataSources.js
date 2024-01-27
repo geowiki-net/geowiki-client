7278d1bcb8b89e0629a1a55776f242c5edb652d4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-01-27 21:21:31 +0100

    EntityList: move code from DataSources

--- src/DataSources.js
+++ src/DataSources.js
@@ -1,5 +1,5 @@
 import OverpassFrontend from 'overpass-frontend'
-import isRelativePath from './isRelativePath'
+import EntityList from './EntityList'
 
 const defaultList = {
   osm: {
@@ -8,74 +8,14 @@ const defaultList = {
   }
 }
 
-module.exports = class DataSources {
+module.exports = class DataSources extends EntityList {
   constructor (app) {
-    this.app = app
-    this._list = null
-    this._sources = {}
+    super(app, app.config.dataSources, defaultList)
   }
 
-  /**
-   * list all available data source
-   * @returns Promise will resolve to a list of data source
-   */
-  list () {
-    return new Promise((resolve, reject) => {
-      if (this._list) {
-        return resolve(this._list)
-      }
-
-      this._list = defaultList
-      if (app.config.dataSources) {
-        this._list = app.config.dataSources.list ?? defaultList
-      }
-
-      Object.entries(this._list).forEach(([id, item]) => {
-        item.id = id
-
-        if (!item.title) {
-          item.title = id
-        }
-      })
-
-      resolve(this._list)
-    })
-  }
-
-  /**
-   * get the datasource with the specified ID
-   * @param string [id] the id of the data source. if null, the default (first in list) data source will be returned. if the data source is not loaded, a new data source with the URL derived from the id will be created.
-   * @returns Promise will resolve to an OverpassFrontend object
-   */
-  get (id) {
-    return new Promise((resolve, reject) => {
-      this.list().then(list => {
-        if (!id) {
-          if (!Object.keys(list).length) {
-            return reject(new Error('no data sources defined'))
-          }
-
-          id = Object.keys(list)[0]
-        }
-
-        if (!(id in list)) { // TODO: maybe need to allow additional data sources
-          list[id] = {
-            id,
-            title: id,
-            url: id
-          }
-        }
-
-        const item = list[id]
-
-        if (!item.dataSource) {
-          const url = (isRelativePath(item.url) ? this.app.config.dataDirectory + '/' : '') + item.url
-          item.dataSource = new OverpassFrontend(url, item.options)
-        }
-
-        resolve(item)
-      })
-      .catch(err => reject(err))
-    })
+  resolveItem (item, url) {
+    if (!item.dataSource) {
+      item.dataSource = new OverpassFrontend(url, item.options)
+    }
   }
 }
---
+++ src/EntityList.js
@@ -0,0 +1,74 @@
+import isRelativePath from './isRelativePath'
+
+module.exports = class EntityList {
+  constructor (app, config, defaultList) {
+    this.app = app
+    this.config = config
+    this.defaultList = defaultList
+    this._list = null
+  }
+
+  /**
+   * list all available data source
+   * @returns Promise will resolve to a list of data source
+   */
+  list () {
+    return new Promise((resolve, reject) => {
+      if (this._list) {
+        return resolve(this._list)
+      }
+
+      this._list = this.defaultList
+      if (this.config) {
+        this._list = this.config.list ?? defaultList
+      }
+
+      Object.entries(this._list).forEach(([id, item]) => {
+        item.id = id
+
+        if (!item.title) {
+          item.title = id
+        }
+      })
+
+      resolve(this._list)
+    })
+  }
+
+  /**
+   * get the datasource with the specified ID
+   * @param string [id] the id of the data source. if null, the default (first in list) data source will be returned. if the data source is not loaded, a new data source with the URL derived from the id will be created.
+   * @returns Promise will resolve to an OverpassFrontend object
+   */
+  get (id) {
+    return new Promise((resolve, reject) => {
+      this.list().then(list => {
+        if (!id) {
+          if (!Object.keys(list).length) {
+            return reject(new Error('no entities defined'))
+          }
+
+          id = Object.keys(list)[0]
+        }
+
+        if (!(id in list)) { // TODO: maybe need to allow additional data sources
+          list[id] = {
+            id,
+            title: id,
+            url: id
+          }
+        }
+
+        const item = list[id]
+
+        if (!item.dataSource) {
+          const url = (isRelativePath(item.url) ? this.app.config.dataDirectory + '/' : '') + item.url
+          this.resolveItem(item, url)
+        }
+
+        resolve(item)
+      })
+      .catch(err => reject(err))
+    })
+  }
+}
