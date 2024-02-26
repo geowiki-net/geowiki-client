2c21a5db017949337c24699c25660ec62a431b72
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-02-26 19:05:33 +0100

    twigGet: if template is not a string, return value

--- src/twigGet.js
+++ src/twigGet.js
@@ -3,6 +3,10 @@ const Twig = require('twig')
 const twigTemplates = {}
 
 module.exports = function twigGet (template, data) {
+  if (typeof template !== 'string') {
+    return template
+  }
+
   if (!(template in twigTemplates)) {
     twigTemplates[template] = Twig.twig({ data: template })
   }
